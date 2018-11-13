e4657c73a572232d23f7fffdbbaa8abc2ba3e895
Tanguy PATTE <tanguy.patte@gmail.com>
2018-11-13 18:52:03 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,26 @@
 from setuptools import find_packages
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ansible-tests',
-    version='0.0.1',
-    packages=find_packages(exclude=["*_tests"]),
+    version='0.0.4',
+    author="Tanguy Patte",
+    url="https://github.com/TanguyPatte/ansible-tests.git",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["*_tests_tests"]),
     install_requires=[
         'click',
-        'pyyaml'
+        'pyyaml',
+        'testinfra',
+        'ansible'
     ],
     entry_points={
         'console_scripts': [
-            'ansible_tests = ansible_tests.__main__:run',
+            'ansible-tests = ansible_tests.__main__:run',
         ],
     },
 )
